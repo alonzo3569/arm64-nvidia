8d0ea1be2fbf4675bb8516da8293cc32ce13c8aa
alonzo3569 agx <r07525074@ntu.edu.tw>
2021-02-10 19:35:49 +0000

    moos-ivp-agx upload

---
+++ moos-ivp-agx/src/pPoseKeepingX/PoseKeepingX.h
@@ -0,0 +1,81 @@
+/************************************************************/
+/*    NAME: Logan                                           */
+/*    ORGN: MIT                                             */
+/*    FILE: PoseKeepingX.h                                  */
+/*    DATE: 2019/11/15                                      */
+/************************************************************/
+
+#ifndef PoseKeepingX_HEADER
+#define PoseKeepingX_HEADER
+
+#include "MOOS/libMOOS/Thirdparty/AppCasting/AppCastingMOOSApp.h"
+#include "PoseKeeping_Mode.h"
+#include <memory>
+
+class PoseKeepingX : public AppCastingMOOSApp
+{
+ public:
+  PoseKeepingX();
+  ~PoseKeepingX() {};
+
+ protected: // Standard MOOSApp functions to overload
+  bool OnNewMail(MOOSMSG_LIST &NewMail);
+  bool Iterate();
+  bool OnConnectToServer();
+  bool OnStartUp();
+
+ protected: // Standard AppCastingMOOSApp function to overload
+  bool buildReport();
+
+ protected:
+  void registerVariables();
+
+ protected: // My function
+  double  Distance(double, double, double, double);
+  void    CheckMode(const Mode);
+  double  CheckSpeed(double);
+
+  //10.21
+  void    ShowCompassHeading();
+  void    PostPolygons(string mode = "");
+  bool    Filter();
+
+ protected: // Mail Callbacks
+#if 0 // Keep this as an example for callbacks
+  bool onMessageFoo(CMOOSMsg&);
+#endif
+
+ private: // Configuration variables
+  std::string m_switch_mode;
+  double m_previous_time;
+  double m_previous_error;
+  double m_steady_error;
+  double m_kp;
+  double m_ki;
+  double m_kd;
+  double m_upper_speed;
+  double m_lower_speed;
+
+ private: // State variables
+  double m_nav_heading;
+  double m_osx;
+  double m_osy;
+  double m_desired_x;
+  double m_desired_y;
+  double m_desired_heading;
+  bool   m_active;
+  bool   m_keep_heading;
+  double m_arrival_radius;
+  double m_tolerance_radius; 
+  double m_distance;  //for report
+  double m_gps_heading; //for report
+  Mode   mode;
+
+  // 10.21experiment feature
+  double m_pre_heading;
+
+  // 11.25experiment
+  std::string m_sim;
+};
+
+#endif
---
+++ moos-ivp-agx/src/pPoseKeepingX/PoseKeepingX_Info.cpp
@@ -0,0 +1,128 @@
+/****************************************************************/
+/*   NAME: Logan                                                */
+/*   ORGN: MIT                                                  */
+/*   FILE: PoseKeepingX_Info.cpp                                */
+/*   DATE: 2019/11/15                                           */
+/****************************************************************/
+
+#include <cstdlib>
+#include <iostream>
+#include "PoseKeepingX_Info.h"
+#include "ColorParse.h"
+#include "ReleaseInfo.h"
+
+using namespace std;
+
+//----------------------------------------------------------------
+// Procedure: showSynopsis
+
+void showSynopsis()
+{
+  blk("SYNOPSIS:                                                       ");
+  blk("------------------------------------                            ");
+  blk("  The pPoseKeepingX application is used for holding Heron's     ");
+  blk("  position and heading as much as possible without using Helm.  ");
+  blk("  This is a capability suitable for multi-vehicle testing.      ");
+  blk("  With pPoseKeeping application, we can deploy N herons out to  ");
+  blk("  station-keeping points just off the dock, putting them into a ");
+  blk("  pose-keeping mode (Helm not active), deploying the mission    ");
+  blk("  which would halt the pose-keeping mode.                       ");
+  blk("                                                                ");
+}
+
+//----------------------------------------------------------------
+// Procedure: showHelpAndExit
+
+void showHelpAndExit()
+{
+  blk("                                                                ");
+  blu("=============================================================== ");
+  blu("Usage: pPoseKeepingX file.moos [OPTIONS]                   ");
+  blu("=============================================================== ");
+  blk("                                                                ");
+  showSynopsis();
+  blk("                                                                ");
+  blk("Options:                                                        ");
+  mag("  --alias","=<ProcessName>                                      ");
+  blk("      Launch pPoseKeepingX with the given process name         ");
+  blk("      rather than pPoseKeepingX.                           ");
+  mag("  --example, -e                                                 ");
+  blk("      Display example MOOS configuration block.                 ");
+  mag("  --help, -h                                                    ");
+  blk("      Display this help message.                                ");
+  mag("  --interface, -i                                               ");
+  blk("      Display MOOS publications and subscriptions.              ");
+  mag("  --version,-v                                                  ");
+  blk("      Display the release version of pPoseKeepingX.        ");
+  blk("                                                                ");
+  blk("Note: If argv[2] does not otherwise match a known option,       ");
+  blk("      then it will be interpreted as a run alias. This is       ");
+  blk("      to support pAntler launching conventions.                 ");
+  blk("                                                                ");
+  exit(0);
+}
+
+//----------------------------------------------------------------
+// Procedure: showExampleConfigAndExit
+
+void showExampleConfigAndExit()
+{
+  blk("                                                                ");
+  blu("=============================================================== ");
+  blu("pPoseKeepingX Example MOOS Configuration                        ");
+  blu("=============================================================== ");
+  blk("                                                                ");
+  blk("ProcessConfig = pPoseKeepingX                                   ");
+  blk("{                                                               ");
+  blk("  AppTick   = 4                                                 ");
+  blk("  CommsTick = 4                                                 ");
+  blk("  position  = 20,-15                                            ");
+  blk("  heading   = 180                                               ");
+  blk("  Kp        = 1.2                                               ");
+  blk("  Kd        = 0.0                                               ");
+  blk("  Ki        = 0.0                                               ");
+  blk("  tolerance_radius = 5                                          ");
+  blk("  sim       = false                                             ");
+  blk("}                                                               ");
+  blk("                                                                ");
+  exit(0);
+}
+
+
+//----------------------------------------------------------------
+// Procedure: showInterfaceAndExit
+
+void showInterfaceAndExit()
+{
+  blk("                                                                ");
+  blu("=============================================================== ");
+  blu("pPoseKeepingX INTERFACE                                         ");
+  blu("=============================================================== ");
+  blk("                                                                ");
+  showSynopsis();
+  blk("                                                                ");
+  blk("SUBSCRIPTIONS:                                                  ");
+  blk("------------------------------------                            ");
+  blk("  NAV_HEADING_CPNVG              = [0,359]                      ");
+  blk("  NAV_HEADING                    = [0,359]                      ");
+  blk("  NAV_X                          = [-inf/+inf]                  ");
+  blk("  NAV_Y                          = [-inf/+inf]                  ");
+  blk("  THRUST_MODE_DIFFERENTIAL       = [true/false]                 ");
+  blk("                                                                ");
+  blk("PUBLICATIONS:                                                   ");
+  blk("------------------------------------                            ");
+  blk("  DESIRED_THRUST_L   		= [-100,100]                   ");
+  blk("  DESIRED_THRUST_R   		= [-100,100]                   ");
+  blk("                                                                ");
+  exit(0);
+}
+
+//----------------------------------------------------------------
+// Procedure: showReleaseInfoAndExit
+
+void showReleaseInfoAndExit()
+{
+  showReleaseInfo("pPoseKeepingX", "mit");
+  exit(0);
+}
+
