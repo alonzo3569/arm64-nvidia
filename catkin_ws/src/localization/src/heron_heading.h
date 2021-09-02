693fbeb31b126ab0fa9d76ba02546252bde33077
logan heron agx <r07525074@ntu.edu.tw>
2021-09-02 11:17:36 +0000

    0902 heron agx update

--- catkin_ws/src/localization/src/heron_heading.h
+++ catkin_ws/src/localization/src/heron_heading.h
@@ -1,6 +1,7 @@
 #include <ros/ros.h>
 #include <std_msgs/Float64.h>
 #include <sensor_msgs/Imu.h>
+#include "ntu_msgs/Tdoa.h"
 
 class Localization
 {
@@ -9,9 +10,11 @@ class Localization
 
         // Publisher
         ros::Publisher pub_NavHdg;
+        ros::Publisher pub_TdoaAngle;
 
         // Subscriber
         ros::Subscriber sub_imu;
+        ros::Subscriber sub_tdoa;
 
     public:
 	// Data
@@ -21,6 +24,7 @@ class Localization
         Localization();
         ~Localization();
         void callback_imu(const sensor_msgs::Imu msg);
+	void callback_tdoa(const ntu_msgs::Tdoa::ConstPtr& msg);
 	std_msgs::Float64 toRosFloat(double value);
 	std_msgs::Float64 toMoosAngle(double value);
 };
